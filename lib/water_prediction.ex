73ab9f4f617d2a9be49dccf7cf1fe3ce5060ca5b
gwrxuk <gwrx2005@gmail.com>
2024-07-06 01:05:11 +0800

    Update water_prediction.ex

--- lib/water_prediction.ex
+++ lib/water_prediction.ex
@@ -5,4 +5,14 @@ defmodule WaterPrediction do
     |> WaterLevelParser.parse_stream()
     |> Enum.to_list()
   end
+
+  def read_csv_row_by_row(file_path) do
+    file_path
+    |> File.stream!()
+    |> WaterLevelParser.parse_stream
+    |> Stream.map(fn [date, level] ->
+      %{date: :binary.copy(date), level: level}
+    end)
+    |>Stream.run
+  end
 end
